e42cb81e09f159e66bc0cbbd65c55f79725ad141
dima <dimitori.g@gmail.com>
2023-07-04 16:06:08 +0900

    add chakra table for represent glyph info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,110 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import fetchData from '../api/base';
 import { Glyph } from '../types/glyph';
 import { apiEndPoints } from '../data/api';
-import GlyphCard from '../components/GlyphCard';
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
+
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
+
+import { Button } from '@chakra-ui/react';
+
+import { Input } from '@chakra-ui/react';
 
 const Glyph = () => {
-  const [glyph, setGlyph] = useState<Glyph[]>([]);
+  const [glyphs, setGlyphs] = useState<Glyph[] | null>(null);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [phonetic, setPhonetic] = useState('');
 
   const fetchGlyph = async () => {
-    const glyphResponse = await fetchData(apiEndPoints.glyph);
+    if (!phonetic) {
+      setError('Need at last one parameter.');
+      return;
+    }
+    setGlyphs(null);
+    setIsLoading(true);
+    setError('');
+    const glyphResponse = await fetchData(apiEndPoints.glyph, {
+      phonetic: phonetic,
+    });
     if (glyphResponse.status === 200) {
-      setGlyph(glyphResponse.data);
+      setGlyphs(glyphResponse.data);
     } else {
       setError(glyphResponse.message);
     }
+    setIsLoading(false);
   };
-
-  useEffect(() => {
-    fetchGlyph();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (!glyph.length) return <div>{error}</div>;
 
   return (
     <>
       <div>Glyphs</div>
-      {glyph.map((glyph) => (
-        <GlyphCard key={glyph.id} glyph={glyph}></GlyphCard>
-      ))}
+      <Input
+        value={phonetic}
+        onChange={(e) => setPhonetic(e.target.value)}
+        placeholder="Phonetic"
+        width="auto"
+      />
+      <Button isLoading={isLoading} isActive={!isLoading} onClick={fetchGlyph}>
+        Search
+      </Button>
+      {glyphs && glyphs.length > 0 && (
+        <TableContainer padding={10}>
+          <Table variant="striped" colorScheme="gray">
+            <TableCaption>Glyph Table</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Glyph</Th>
+                <Th>Phonetic</Th>
+                <Th>Cantonese</Th>
+                <Th>Pinyin</Th>
+                <Th>Korean</Th>
+                <Th>Onyomi</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {glyphs?.map((glyph) => (
+                <Tr key={glyph.id}>
+                  <Td>{glyph.glyph}</Td>
+                  <Td>{glyph.phonetic}</Td>
+                  <Td>{glyph.cantonese}</Td>
+                  <Td>{glyph.pinyin}</Td>
+                  <Td>{glyph.korean}</Td>
+                  <Td>{glyph.on}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      )}
+      {glyphs?.length === 0 && (
+        <Alert status="info">
+          <AlertIcon />
+          <AlertTitle>Info</AlertTitle>
+          <AlertDescription>Data not found</AlertDescription>
+        </Alert>
+      )}
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>Error!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
     </>
   );
 };
